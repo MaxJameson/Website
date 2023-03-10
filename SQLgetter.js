a1f99f0d6f20081a4d88990efa453bb6a2f2af6c
MaxJameson <maxjameson19999@live.co.uk>
2023-03-10 12:53:23 +0000

    Profile page now shows user specific images

--- SQLgetter.js
+++ SQLgetter.js
@@ -1,10 +1,42 @@
 // calls a php script to fetch images from a database
 async function getPoints() {
-    await fetch('getPhotos.php').then((res) => res.json())
+
+    
+
+    // stores the name of the current page
+    url = document.URL;
+    file = url.substring(url.lastIndexOf('/')+1);
+    console.log(file);
+
+    // checks if the user is on their profile page
+    if(file == "profile.html"){
+
+        // fetches current profiles photos
+        return photoFetch(sessionStorage.getItem("userID"));
+
+    }
+
+    return photoFetch(" ");
+}
+
+// submits a fetch request for photos from the database
+async function photoFetch(id){
+
+    // create a form to store user ID
+    const photoForm = new FormData();
+
+    // adds user ID to form
+    photoForm.append('id',id)
+
+    // fetches photos
+    await fetch('getPhotos.php',{
+        method: "post",
+        body: photoForm
+    }).then((res) => res.json())
     .then(response => {
         photos = response;
-    }).catch(error => console.log(error)) 
-    
+    }).catch(error => console.log(error))
+        
     // returns an object containing records
-    return photos;
+    return photos; 
 }
--- getPhotos.php
+++ getPhotos.php
@@ -8,8 +8,19 @@ $db = 'bitmap';
 // estavlishes a connection
 $db = new mysqli ('localhost', $user, $pass, $db) or die("unable to connect");
 
-// runs a query to select images
-$query = mysqli_query($db, "SELECT tblphotos.PhotoName, tblphotos.StoragePath, tblphotos.Date, tblphotos.Lat, tblphotos.Long, tblprofiles.UserName FROM tblphotos INNER JOIN tblprofiles ON tblphotos.UserID = tblprofiles.UserID");
+// stores name of the image
+$id = $_POST["id"];
+
+// checks if a value has been submitted for an ID
+if($id == " "){
+    // runs a query to select all images
+    $query = mysqli_query($db, "SELECT tblphotos.PhotoName, tblphotos.StoragePath, tblphotos.Date, tblphotos.Lat, tblphotos.Long, tblprofiles.UserName FROM tblphotos INNER JOIN tblprofiles ON tblphotos.UserID = tblprofiles.UserID");
+}
+else{
+    // runs a query to select profile specific images
+    $query = mysqli_query($db, "SELECT tblphotos.PhotoName, tblphotos.StoragePath, tblphotos.Date, tblphotos.Lat, tblphotos.Long, tblprofiles.UserName FROM tblphotos INNER JOIN tblprofiles ON tblphotos.UserID = tblprofiles.UserID WHERE tblprofiles.UserID = '$id'");
+}
+
 
 // stores the query result
 $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
